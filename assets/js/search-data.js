fc45a7cca44733dc21706d175553727b80cc96a8
ebarkhordar <realbarkhordar@gmail.com>
2026-02-12 15:43:28 +0000

    Deploying to gh-pages from @ ebarkhordar/ebarkhordar.github.io@f3b6732af368f6e6617b2efd5577782f5f6525ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,7 +35,7 @@ ninja.data = [{
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/repositories/";
+            window.location.href = "/https:/github.com/ebarkhordar?tab=repositories";
           },
         },{id: "nav-cv",
           title: "CV",
@@ -51,28 +51,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         

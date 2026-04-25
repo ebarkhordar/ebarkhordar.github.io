5a72358c49c2e9554a4ea1d70daed5cec2c125df
ebarkhordar <realbarkhordar@gmail.com>
2026-04-25 09:15:47 +0000

    Deploying to gh-pages from @ ebarkhordar/ebarkhordar.github.io@aca6b4eaca4095a8205193cc24f38528ec396a91 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Selected research and engineering projects in NLP and machine learning.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
